0084162a9db7067babd2f6505c9a9e68b782989a
Tiago Tiede <geschopf@gmail.com>
2017-11-05 21:21:29 -0200

    Create Packge with exception

---
+++ src/main/java/br/com/matera/sge/exception/ServiceException.java
@@ -0,0 +1,13 @@
+package br.com.matera.sge.exception;
+
+import org.apache.log4j.Logger;
+
+public class ServiceException extends Exception {
+
+	private static Logger logger = Logger.getLogger(ServiceException.class);
+	
+	public ServiceException(String message, Exception e) {
+		logger.error(message, e);
+	}
+
+}
