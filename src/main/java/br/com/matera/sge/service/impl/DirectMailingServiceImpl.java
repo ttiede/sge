189092aaaf07f153369e9a93012efda3e1fa8a6c
Tiago Tiede <geschopf@gmail.com>
2017-11-06 17:21:52 -0200

    Refactor to remove a block of IF

--- src/main/java/br/com/matera/sge/service/impl/DirectMailingServiceImpl.java
+++ src/main/java/br/com/matera/sge/service/impl/DirectMailingServiceImpl.java
@@ -35,24 +35,23 @@ public class DirectMailingServiceImpl implements DirectMailingService {
 		List<DirectMailing> mailingsElegileStudents = new ArrayList();
 		try {
 			List<Student> students = studentService.retrieveAllStudents();
+
 			for (final DirectMailing directMailing : mailings) {
 
-				List<Student> document = students.stream()
+				List<Student> elegileStudent = students.stream()
 						.filter(s -> s.getName().equals(directMailing.getName())
 								&& s.getAddress().equals(directMailing.getAddress())
 								&& s.getZipCode().equals(directMailing.getZipCode()))
 						.collect(Collectors.toList());
 
-				if (!document.isEmpty() && document != null) {
-					if (!String.valueOf(document.get(0).getDocument()).isEmpty()) {
-						Course course = courseService
-								.retrieveCourseOfStudent(String.valueOf(document.get(0).getDocument()));
-						if (course != null) {
-							Stream<Double> score = course.getScore().values().stream()
-									.filter(c -> Double.parseDouble(String.valueOf(c)) < Double.parseDouble("7.0"));
-							if(score != null) {
-								mailingsElegileStudents.add(directMailing);
-							}
+				if (elegileStudent != null && elegileStudent.size() > 0) {
+					String documentStudente = elegileStudent.get(0).getDocument();
+					Course course = courseService.retrieveCourseOfStudent(String.valueOf(documentStudente));
+					if (course != null) {
+						Stream<Double> score = course.getScore().values().stream()
+								.filter(c -> Double.parseDouble(String.valueOf(c)) < Double.parseDouble("7.0"));
+						if (score != null) {
+							mailingsElegileStudents.add(directMailing);
 						}
 					}
 
