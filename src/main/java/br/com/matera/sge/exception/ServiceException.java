91e32fb8f327084427eac1738b500c34305c4023
Tiago Tiede <geschopf@gmail.com>
2017-11-06 14:47:31 -0200

    Service Exception

--- src/main/java/br/com/matera/sge/exception/ServiceException.java
+++ src/main/java/br/com/matera/sge/exception/ServiceException.java
@@ -5,7 +5,7 @@ import org.apache.log4j.Logger;
 public class ServiceException extends Exception {
 
 	private static Logger logger = Logger.getLogger(ServiceException.class);
-	
+
 	public ServiceException(String message, Exception e) {
 		logger.error(message, e);
 	}
